9d32f3f426640c9ba51f8a62fc45c30143a388bc
Cordes-RJ <richardj.cordes@gmail.com>
2020-06-06 19:20:08 -0400

    Update hash.py

--- src/Eliex/hash.py
+++ src/Eliex/hash.py
@@ -2,11 +2,14 @@
 
 import hashlib
 
+# getMD5 returns an MD5 hash of a file, will return "" on error.
 def getMD5(filepath):
     with open(filepath, 'rb') as file:
         data = file.read()
+    print("Unable to read: " + filepath)
     return hashlib.md5(data).hexdigest()
     
 """
 x = getMD5('config.ini') out: de88ec9491a8b87697751db5cc5b2fd1
 """
+
