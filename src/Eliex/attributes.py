e06763755032e8c77d6fc79042c979a1bfec8fca
Cordes-RJ <richardj.cordes@gmail.com>
2020-06-07 18:09:01 -0400

    Added extension framework
    added paperpile and default extensions to attribute manager as well as a generalized framework to allow for more extensions enabling more flexible inputs beyond .ini and paperpile copy/paste in the future.

--- src/Eliex/attributes.py
+++ src/Eliex/attributes.py
@@ -11,20 +11,43 @@ class AttributeManager(configparser.ConfigParser):
         # from particular formats such as JSON or Paperpile which cannot be
         # read using the attribute manager's default reader which is intended
         # to read ini files.
-        #self.extensions['paperpile'] = ...
+        self.extensions['paperpile'] = paperpileExt
+        self.extensions['default'] = defaultExt
     def setNewSection(self, SectionName):
         self.currentSection = "[" + SectionName + "]"
-    def readIn(self,String):
-        self.read_string(self.currentSection+"\n" + String)
+    def readIn(self,String,**kwargs):
+        Extension = "default"
+        if "extension" in kwargs.keys():
+            Extension = kwargs["extension"]
+        self.read_string(self.currentSection+"\n" + self.extensions[Extension](String))
+
+# default extension simply returns 
+def defaultExt(s):
+    return s
+
+# paperpileExt is an extension meant to prep a paperpile string
+def paperpileExt(s):
+    ind1 = s.find('\n')
+    ind2 = s.rfind('\n')
+    return s[ind1+1:ind2]
     
 """
+# set new section test
 a = AttributeManager()
 a.readIn("x = 32")
 a.setNewSection("Test")
 a.readIn("z = 47")
-"""
-"""
+a['Test']['z'] = 47
+
+# simple test
 a = AttributeManager()
-a.readIn(input("please input: "))
+a.readIn("x = 32")
+a.readIn("b = 17")
+x = a['DEFAULT']['b']
+
+# paperpile test
+a = AttributeManager()
+a.readIn(input("please input: "), extension="paperpile")
 x = [a]
+
 """
