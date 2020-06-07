5d6477457ffb10153b358ff301d499b8085acc41
Cordes-RJ <richardj.cordes@gmail.com>
2020-06-07 17:48:31 -0400

    Listener Tested
    Listener works and is tested.

--- src/Eliex/listener.py
+++ src/Eliex/listener.py
@@ -1,28 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import os
+from time import sleep
 
 # Listener listens for changes in a folder
 class Listener:
-    def __init__(self, folder, configmngr):
-        self.folder = folder
-        self.configmngr = configmngr
-    def getPaths(self):
+    def __init__(self, folder, tick):
+        self.folder = folder # directory to be listened to
+        self.tick = tick # seconds between checks on directory
+    def getPaths(self): # returns all paths in a directory
         paths = []
         for path in os.listdir(self.folder):
             fullpath = os.path.join(self.folder,path)
             if os.path.isfile(fullpath):
                 paths.append(fullpath)
         return paths
+    def SetTick(self, tick): # update function for tick
+        self.tick = tick
     def Listen(self):
         while True:
-            time.sleep(self.configmngr['DEFAULT']['ListenTick'])
+            print("tick...")
+            paths = self.getPaths()
+            if len(paths) != 0:
+                return paths
+            sleep(self.tick)
             
-    
-
+            
 """
-import config
-c = config.ConfigManager('config.ini')
-L = Listener("testfolder", c)
-x = L.getPaths()
+L = Listener("testfolder",5)
+x = L.Listen()
 """
