fc2b036288d72762b40b5b8570c44cf3f12cbd53
Cordes-RJ <richardj.cordes@gmail.com>
2020-06-07 22:18:03 -0400

    Reorganize general file functions into utilFile, add check for file existence function

---
+++ src/Eliex/utilFile.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+# Get Extension of file from filepath string 
+def getExt(filepath):
+    i = filepath.rindex(".")
+    return filepath[i:len(filepath)]
+
+"""
+print(getExt("testfolder\test.txt"))
+"""
+
+# editFilePath edits the filepath of a file at a given filepath
+def editFilePath(filepath, newFilepath):
+    os.rename(filepath, newFilepath)
+
+"""
+renameFile("otherTest.txt", "testfolder\otherTest.pdf")
+"""
+
+# makeFile takes a string and creates a file given a filepath
+def makeFile(filepath, string):
+    with open(filepath,"a") as file:
+        file.write(string)
+
+# checkForFile checks if a file exists or not, returns a bool
+def checkForFile(filepath):
+    return os.path.exists(filepath)
