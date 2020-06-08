955bdfd49b4048873b62b3175ec09c86e3c9465e
Cordes-RJ <richardj.cordes@gmail.com>
2020-06-08 11:47:58 -0400

    add comments to functions

--- src/Eliex/hermes.py
+++ src/Eliex/hermes.py
@@ -8,17 +8,19 @@ import config
 
 #fh = fileHandler.FileHandler("testfolder","testfolder/import/test.txt")
 
-    
+# loadingConfigs loads configs and returns a config manager to loop
 def loadingConfigs():
     print("Loading configuration...")
     return config.ConfigManager("config.ini")
 
+# listening notifies user of status and listens for new files
 def listening(folder,tick):
-    print("Creating a Listening Post...")
+    print("Opening listening post...")
     L = listener.Listener(folder, tick)
     print("Listening...")
     return L.Listen()
 
+# foundAFilePrompt asks a user to confirm integration of a file
 def foundAFilePrompt(fh):
     print("Eliex found a file: " + utilFile.getFileName(fh.filePath)
     + "\nShould we integrate it?")
@@ -27,19 +29,40 @@ def foundAFilePrompt(fh):
     moveToFailedImport(fh)
     return False
 
+# moveToFailedImport moves a canceled or failed file to another folder
 def moveToFailedImport(fh):
     print("Moving file to failedImport!")
     fh.moveToFailedImport()
 
-#%%
+# checkingForDuplicate checks for a duplicate hash
+    # returns True if no duplicate found, else false.
+def checkingForDuplicate(fh):
+    #fh = fileHandler.FileHandler("testfolder","testfolder/import/test.txt")
+    b = fh.checkHash()
+    if b:
+        print("We already have this file!")
+        print("Search in obsidian for the hash: " +fh.hash)
+        moveToFailedImport(fh)
+        return False
+    else:
+        True
 
+
+#%%
+# mainLoop runs a main prompt loop for file import and listener
+        # returns used in lieu of while loop for readability and clarity of
+        # exit conditions.
 def mainLoop(C):
     F = listening(C['DEFAULT']['Repo']+"/import",int(C['DEFAULT']['ListenTick'])) # listen until file-found
     fh = fileHandler.FileHandler(C['DEFAULT']['Repo'],F) # instantiate filehandler
     B = foundAFilePrompt() # ask user to confirm integration of file
     if B == False:
-        moveToFailedImport(fh) # notify user of file transfer to failedImport
         return # exit loop
+    B = checkingForDuplicate(fh)
+    if B == False:
+        return # exit loop
+    
+    
     
     
 C = loadingConfigs()
