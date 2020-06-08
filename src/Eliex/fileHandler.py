8d28c7e2b16714a4d44c7ed85e24cc9b8c3bf4b4
Cordes-RJ <richardj.cordes@gmail.com>
2020-06-07 22:07:36 -0400

    Added makeFile function

--- src/Eliex/fileHandler.py
+++ src/Eliex/fileHandler.py
@@ -11,10 +11,16 @@ def getExt(filepath):
 print(getExt("testfolder\test.txt"))
 """
 
-# Rename file
+# editFilePath edits the filepath of a file at a given filepath
 def editFilePath(filepath, newFilepath):
     os.rename(filepath, newFilepath)
-    
+
 """
 renameFile("otherTest.txt", "testfolder\otherTest.pdf")
 """
+
+# makeFile takes a string and creates a file given a filepath
+def makeFile(filepath, string):
+    with open(filepath,"a") as file:
+        file.write(string)
+
